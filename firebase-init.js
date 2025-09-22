c1ebe67114eafcf77c5953f5a41fdbcb530134f1
Haziq Fauzi <G4M1NG.6UY@gmail.com>
2025-09-22 21:42:12 +0800

    Initialize Firebase with configuration and services
    This file initializes Firebase with the provided configuration and sets up analytics, authentication, and Firestore references.

---
+++ firebase-init.js
@@ -0,0 +1,23 @@
+// In firebase-init.js
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-analytics.js";
+
+// PASTE YOUR COPIED firebaseConfig OBJECT HERE
+const firebaseConfig = {
+    apiKey: "AIzaSyAZJRs8Otce7v1Wex2iW6NuKxoFkouybsI",
+    authDomain: "igcse-computer-science-quiz.firebaseapp.com",
+    projectId: "igcse-computer-science-quiz",
+    storageBucket: "igcse-computer-science-quiz.firebasestorage.app",
+    messagingSenderId: "1087878300052",
+    appId: "1:1087878300052:web:c6823f3bf306e7fb0b8f81",
+    measurementId: "G-T296MN9K4M"
+  };
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Get references to the services we're going to use
+const auth = firebase.auth();
+const db = firebase.firestore();
