95ef934ade7ba3c76172be05d32322189daacc55
Haziq Fauzi <G4M1NG.6UY@gmail.com>
2025-09-22 22:14:58 +0800

    Add Firestore and Auth imports to firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,6 +1,8 @@
 // In firebase-init.js
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-analytics.js";
 
 // PASTE YOUR COPIED firebaseConfig OBJECT HERE
